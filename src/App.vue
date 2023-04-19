3092ee6ac6fcc82de9581877faa256cf01c9dc59
MLibralato91 <michelelibralato911@gmail.com>
2023-04-19 10:58:56 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-
+    <h1>Ciao
+      
+    </h1>
   </div>
 </template>
 
 <script>
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import MainComponent from './components/MainComponent.vue';
+  import FooterComponent from './components/FooterComponent.vue';
   export default {
-    
+    components:{
+      HeaderComponent,
+      MainComponent,
+      FooterComponent
+    }
   }
 </script>
 
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,7 +6,7 @@
 
 <script>
   export default {
-    name:'MainComponent'
+    name:'FooterComponent'
   }
 </script>
 
