f681ae562e5a0144c2585942957a05ff1a4f38aa
MLibralato91 <michelelibralato911@gmail.com>
2023-04-20 22:16:19 +0200

    update styles cards

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -2,21 +2,17 @@
   <div class="col-12 col-sm-6 col-md-4 col-lg-2">
     <!-- Rotating card -->
     <div class="card-wrapper myCard">
-      <div id="card-1" @click="infoCard =!infoCard" class="card card-rotating text-center">
+      <div id="card-1" @click="infoCard =!infoCard" class="card card-rotating d-flex flex-column justify-content-between align-items-baseline mb-3">
         
         <!-- Front Side -->
         <div class="faceFront" v-if="infoCard">
           
           <!-- Image-->
-          <div class="card-up">
+          <div class="card-up text-center">
             <img class="img-fluid" :src="store.imageUrl + store.imgSize + image" alt="">
           </div>
           
-          <!-- Content -->
-          <div class="card-body">
-            <h4 class="font-weight-bold mb-3">{{ title }}</h4>
-            <p class="font-weight-bold blue-text">{{ orignalTitle }}</p>
-          </div>
+         
         </div>
         <!-- Front Side -->
 
@@ -29,7 +25,7 @@
             <h6 class="font-weight-bold mb-0">{{ vote }}</h6>
             <h5 class="Stars m-0" :style="'--rating: ' + vote + ';' "></h5>
             <hr>
-            <p class="overflow-auto">
+            <p class=" ">
               {{ overview }}
 
             </p>
@@ -61,6 +57,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.myCard{
+  height: 400px;
+  
+}
+.card-body p{
+  overflow-y: auto;
+}
 :root {
   --star-size: 60px;
   --star-color: #fff;
@@ -80,8 +84,9 @@ export default {
     letter-spacing: 3px;
     background: linear-gradient(90deg, var(--star-background) var(--percent), var(--star-color) var(--percent));
     -webkit-background-clip: text;
-    
+    background-clip: text;
     -webkit-text-fill-color: transparent;
+    color: transparent;
   }
   
 }
