63f6324781f90c0b42c45cd5a13a1ae01298ff97
MLibralato91 <michelelibralato911@gmail.com>
2023-04-20 15:17:02 +0200

    add click on card

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -2,41 +2,35 @@
   <div class="col-12 col-sm-6 col-md-4 col-lg-3">
     <!-- Rotating card -->
     <div class="card-wrapper myCard">
-      <div id="card-1" class="card card-rotating text-center">
-
+      <div id="card-1" @click="infoCard =!infoCard" class="card card-rotating text-center">
+        
         <!-- Front Side -->
-        <div class="face front">
-
+        <div class="faceFront" v-if="infoCard">
+          
           <!-- Image-->
           <div class="card-up">
-            <img class="img-fluid" :src="store.imageUrl+store.imgSize+image" alt="">
+            <img class="img-fluid" :src="store.imageUrl + store.imgSize + image" alt="">
           </div>
-
           
-
           <!-- Content -->
           <div class="card-body">
-            <h4 class="font-weight-bold mb-3">{{title}}</h4>
-            <p class="font-weight-bold blue-text">{{orignalTitle}}</p>
-            
+            <h4 class="font-weight-bold mb-3">{{ title }}</h4>
+            <p class="font-weight-bold blue-text">{{ orignalTitle }}</p>
           </div>
         </div>
         <!-- Front Side -->
 
         <!-- Back Side -->
-        <div class="face back">
+        <div class="faceBack" v-else>
           <div class="card-body">
 
             <!-- Content -->
-            <h4 class="font-weight-bold mb-0">{{title}}</h4>
-            <h6 class="font-weight-bold mb-0">{{vote}}</h6>
+            <h4 class="font-weight-bold mb-0">{{ title }}</h4>
+            <h6 class="font-weight-bold mb-0">{{ vote }}</h6>
             <hr>
             <p>
-              {{overview}}
-              <hr>
-              <!-- Social Icons -->
-            
-            
+              {{ overview }}
+
             </p>
           </div>
         </div>
@@ -44,9 +38,9 @@
 
       </div>
     </div>
-  </div>
 
- 
+
+  </div>
 </template>
 
 <script>
@@ -54,16 +48,18 @@ import { store } from '../data/store';
 export default {
   name: 'ItemCard',
   props: [
-    'title', 'orignalTitle', 'language', 'vote', 'image','overview'
+    'title', 'orignalTitle', 'language', 'vote', 'image', 'overview'
   ],
   data() {
     return {
-      store
+      store,
+      infoCard: true
     }
   },
 }
 </script>
 
 <style lang="scss" scoped>
+
 
 </style>
